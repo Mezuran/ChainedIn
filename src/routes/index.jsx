bd32cf9e3389d5cad3d9fe7b528299a928e1ec7e
Mitho Mizuno <mithomizuno@gmail.com>
2026-01-23 12:04:50 +0700

    Adding CI and Edit README.md

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,7 +4,7 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main class="text-center mx-auto text-gray-700 p-4">
-      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">Hello world!</h1>
+      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">Change Something</h1>
       <Counter />
       <p class="mt-8">
         Visit{" "}
@@ -14,7 +14,7 @@ export default function Home() {
         to learn how to build Solid apps.
       </p>
       <p class="my-4">
-        <span>Something right here</span>
+        <span>Home</span>
         {" - "}
         <A href="/about" class="text-sky-600 hover:underline">
           About Page
