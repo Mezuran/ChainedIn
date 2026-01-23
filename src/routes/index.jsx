94d08c892b47d47fa199e20856457e9745a4e08d
Mitho Mizuno <mithomizuno@gmail.com>
2026-01-23 12:09:31 +0700

    Adding CI and Edit README.md

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,7 +4,7 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main class="text-center mx-auto text-gray-700 p-4">
-      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">Change Something</h1>
+      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">Change Something Here</h1>
       <Counter />
       <p class="mt-8">
         Visit{" "}
