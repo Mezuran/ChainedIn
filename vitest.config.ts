c6d8f9afb7e4453118ae686e986ff61a1975d6d8
Mitho Mizuno <mithomizuno@gmail.com>
2026-02-02 10:28:23 +0700

    Removing templates file

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     test: {
         environment: "jsdom",
         globals: true,
-        setupFiles: ["./setupVitest.ts"],
+        setupFiles: ["./setup.vitest.ts"],
         css: false,
         deps: {
             optimizer: {
